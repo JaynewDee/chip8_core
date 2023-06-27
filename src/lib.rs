e9137c09d5553e9b341528b979e21a9f1b1c6ec2
NuminousBlue <jdiehl2236@gmail.com>
2023-06-27 00:09:35 -0500

    flesh remaining ops

--- src/lib.rs
+++ src/lib.rs
@@ -40,6 +40,8 @@ pub struct Emulator {
     sound_timer: u8,
 }
 
+type Digits = (u16, u16, u16, u16);
+
 impl Emulator {
     pub fn new() -> Emulator {
         let mut emulator = Emulator {
@@ -59,6 +61,21 @@ impl Emulator {
         emulator
     }
 
+    pub fn load(&mut self, data: &[u8]) {
+        let start = START_ADDR as usize;
+        let end = (START_ADDR as usize) + data.len();
+
+        self.ram[start..end].copy_from_slice(data);
+    }
+    pub fn get_display(&self) -> &[bool] {
+        &self.screen
+    }
+
+    pub fn keypress(&mut self, idx: usize, pressed: bool) {
+        assert!(idx < 16);
+
+        self.keys[idx] = pressed;
+    }
     fn push(&mut self, val: u16) {
         self.stack[self.stack_pointer as usize] = val;
         self.stack_pointer += 1;
@@ -107,134 +124,366 @@ impl Emulator {
         let digit4 = op & 0x000F;
 
         let digits = (digit1, digit2, digit3, digit4);
+
         // Operations by OpCode
-        match (digit1, digit2, digit3, digit4) {
+        match digits {
             // 0000 - Nop ::: no-op / do nothing
             (0, 0, 0, 0) => return,
             // 00E0 - CLS ::: clear screen
-            (0, 0, 0xE, 0) => {
-                self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
-            }
+            (0, 0, 0xE, 0) => self.op_00e0(),
             // 00EE - Return from Subroutine
-            (0, 0, 0xE, 0xE) => {
-                let ret_address = self.pop();
-                self.program_counter = ret_address;
-            }
+            (0, 0, 0xE, 0xE) => self.op_00ee(),
             // 1NNN - Jump ::: move program counter to given address
-            (1, _, _, _) => {
-                let jump_to = op & 0xFFF;
-                self.program_counter = jump_to;
-            }
+            (1, _, _, _) => self.op_1nnn(op),
             // 2NNN - Call Subroutine
-            (2, _, _, _) => {
-                self.push(self.program_counter);
-
-                self.program_counter = op & 0xFFF;
-            }
+            (2, _, _, _) => self.op_2nnn(op),
             // 3XNN - Skip next if VX == NN ::: if/else, execute line based on condition
-            (3, _, _, _) => {
-                let vx = digit2 as usize;
-                let value = (op & 0xFF) as u8;
-
-                if self.v_registers[vx] == value {
-                    self.program_counter += 2;
-                }
-            }
+            (3, _, _, _) => self.op_3xnn(op, digits),
             // 4XNN - Skip next if VX != NN ::: same as prev but reversed condition
-            (4, _, _, _) => {
-                let vx = digit2 as usize;
-                let value = (op & 0xFF) as u8;
-
-                if self.v_registers[vx] != value {
-                    self.program_counter += 2;
-                }
-            }
+            (4, _, _, _) => self.op_4xnn(op, digits),
             // 5XY0 - Skip next if VX == VY ::: uses 2 v registers, one for each value
-            (5, _, _, 0) => {
-                let vx = digit2 as usize;
-                let vy = digit3 as usize;
-
-                if self.v_registers[vx] == self.v_registers[vy] {
-                    self.program_counter += 2;
-                }
-            }
+            (5, _, _, 0) => self.op_5xy0(digits),
             // 6XNN - VX = NN ::: Assign second-digit v_register to given value
-            (6, _, _, _) => {
-                let v_reg = digit2 as usize;
-                let value = (op & 0xFF) as u8;
-
-                self.v_registers[v_reg] = value;
-            }
+            (6, _, _, _) => self.op_6xnn(op, digits),
             // 7XNN - VX += NN ::: Adds value to VX register
-            (7, _, _, _) => {
-                let v_reg = digit2 as usize;
-                let value = (op & 0xFF) as u8;
-
-                self.v_registers[v_reg] = self.v_registers[v_reg].wrapping_add(value);
-            }
+            (7, _, _, _) => self.op_7xnn(op, digits),
             // 8XY0 - VX = VY ::: Assigns value from VY register to VX register
-            (8, _, _, 0) => {
-                let v_reg_x = digit2 as usize;
-                let v_reg_y = digit3 as usize;
-
-                self.v_registers[v_reg_x] = self.v_registers[v_reg_y]
-            }
+            (8, _, _, 0) => self.op_8xy0(digits),
             // 8XY1 - Bitwise OR operation
-            (8, _, _, 1) => {
-                let v_reg_x = digit2 as usize;
-                let v_reg_y = digit3 as usize;
-
-                self.v_registers[v_reg_x] |= self.v_registers[v_reg_y];
-            }
+            (8, _, _, 1) => self.op_8xy1(digits),
             // 8XY2 - Bitwise AND operation
-            (8, _, _, 2) => {
-                let v_reg_x = digit2 as usize;
-                let v_reg_y = digit3 as usize;
-
-                self.v_registers[v_reg_x] &= self.v_registers[v_reg_y];
-            }
+            (8, _, _, 2) => self.op_8xy2(digits),
             // 8XY3 - Bitwise XOR operation
-            (8, _, _, 3) => {
-                let v_reg_x = digit2 as usize;
-                let v_reg_y = digit3 as usize;
-
-                self.v_registers[v_reg_x] ^= self.v_registers[v_reg_y];
-            }
+            (8, _, _, 3) => self.op_8xy3(digits),
             // 8XY4 - VX += VY ::: Store carry flag (1 or 0) in VF (flag) register after add
-            (8, _, _, 4) => {
-                let vx = digit2 as usize;
-                let vy = digit3 as usize;
-
-                let (new_vx, carry) = self.v_registers[vx].overflowing_add(self.v_registers[vy]);
-                let new_vf = if carry { 1 } else { 0 };
-
-                self.v_registers[vx] = new_vx;
-                self.v_registers[0xF] = new_vf;
-            }
+            (8, _, _, 4) => self.op_8xy4(digits),
             // 8XY5 - VX -= VY ::: Store carry(borrow) flag in VF register after subtraction
-            (8, _, _, 5) => {
-                let vx = digit2 as usize;
-                let vy = digit3 as usize;
-
-                let (new_vx, borrow) = self.v_registers[vx].overflowing_sub(self.v_registers[vy]);
-                let new_vf = if borrow { 0 } else { 1 };
-
-                self.v_registers[vx] = new_vx;
-                self.v_registers[0xF] = new_vf;
-            }
+            (8, _, _, 5) => self.op_8xy5(digits),
             // 8XY6 - VX >>= 1 ::: Right shift value in VX register, store dropped bit in VF
-            (8, _, _, 6) => self.opcode_8xy6(digits)
+            (8, _, _, 6) => self.op_8xy6(digits),
+            // 8XY7 - VX = VY - VX
+            (8, _, _, 7) => self.op_8xy7(digits),
+            // 8XYE - VX <<= 1
+            (8, _, _, 0xE) => self.op_8xye(digits),
+            // 9XY0 - Skip if VX != VY
+            (9, _, _, 0) => self.op_9xy0(digits),
+            // ANNN - I = NNN ::: Set I-register to nnn value
+            (0xA, _, _, _) => self.op_annn(op),
+            // BNNN - Jump to V0 + NNN
+            (0xB, _, _, _) => self.op_bnnn(op),
+            // CXNN - VX = random() & NN
+            (0xC, _, _, _) => self.op_cxnn(op, digits),
+            // DXYN - Draw Sprite
+            (0xD, _, _, _) => self.op_dxyn(digits),
+            // EX9E - Skip if Key Pressed
+            (0xE, _, 9, 0xE) => self.op_ex9e(digits),
+            // EXA1 - Skip if Key Not Presssed
+            (0xE, _, 0xA, 1) => self.op_exa1(digits),
+            // FX07 - VX = DT
+            (0xF, _, 0, 7) => self.op_fx07(digits),
+            // FX0A - Wait for Key Press
+            (0xF, _, 0, 0xA) => self.op_fx0a(digits),
+            // FX15 - DT = VX
+            (0xF, _, 1, 5) => self.op_fx15(digits),
+            // FX18 - ST = VX
+            (0xF, _, 1, 8) => self.op_fx18(digits),
+            // FX1E - I += VX
+            (0xF, _, 1, 0xE) => self.op_fx1e(digits),
+            // FX29 - Set I to Font Address
+            (0xF, _, 2, 9) => self.op_fx29(digits),
+            // FX33 - I = Binary-Coded Decimal of VX
+            (0xF, _, 3, 3) => self.op_fx33(digits),
+            // FX55 - Store V0 - VX into I
+            (0xF, _, 5, 5) => self.op_fx55(digits),
+            // FX65 - Load I into V0 - VX
+            (0xF, _, 6, 5) => self.op_fx65(digits),
+            // Wildcard
             (_, _, _, _) => unimplemented!("Unimplemented opcode: {}", &op),
-            //
         }
     }
 
-    fn opcode_8xy6(&mut self, digits: (u16, u16, u16, u16)) {
+    // A Function for each Opcode
+    fn op_00e0(&mut self) {
+        self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+    }
+
+    fn op_00ee(&mut self) {
+        self.program_counter = self.pop();
+    }
+
+    fn op_1nnn(&mut self, op: u16) {
+        self.program_counter = op & 0xFFF;
+    }
+
+    fn op_2nnn(&mut self, op: u16) {
+        self.push(self.program_counter);
+        self.program_counter = op & 0xFFF;
+    }
+
+    fn op_3xnn(&mut self, op: u16, digits: Digits) {
+        let vx = digits.1 as usize;
+        let value = (op & 0xFF) as u8;
+
+        if self.v_registers[vx] == value {
+            self.program_counter += 2;
+        }
+    }
+
+    fn op_4xnn(&mut self, op: u16, digits: Digits) {
+        let vx = digits.1 as usize;
+        let value = (op & 0xFF) as u8;
+
+        if self.v_registers[vx] != value {
+            self.program_counter += 2;
+        }
+    }
+
+    fn op_5xy0(&mut self, digits: Digits) {
+        let (vx, vy) = (digits.1 as usize, digits.2 as usize);
+
+        if self.v_registers[vx] == self.v_registers[vy] {
+            self.program_counter += 2;
+        }
+    }
+
+    fn op_6xnn(&mut self, op: u16, digits: Digits) {
+        let vx = digits.1 as usize;
+        let value = (op & 0xFF0) as u8;
+
+        self.v_registers[vx] = value;
+    }
+
+    fn op_7xnn(&mut self, op: u16, digits: Digits) {
+        let vx = digits.1 as usize;
+        let value = (op & 0xFF) as u8;
+
+        self.v_registers[vx] = self.v_registers[vx].wrapping_add(value);
+    }
+
+    fn op_8xy0(&mut self, digits: Digits) {
+        let (vx, vy) = (digits.1 as usize, digits.2 as usize);
+
+        self.v_registers[vx] = self.v_registers[vy];
+    }
+
+    fn op_8xy1(&mut self, digits: Digits) {
+        let (vx, vy) = (digits.1 as usize, digits.2 as usize);
+
+        self.v_registers[vx] |= self.v_registers[vy];
+    }
+
+    fn op_8xy2(&mut self, digits: Digits) {
+        let (vx, vy) = (digits.1 as usize, digits.2 as usize);
+
+        self.v_registers[vx] &= self.v_registers[vy];
+    }
+
+    fn op_8xy3(&mut self, digits: Digits) {
+        let (vx, vy) = (digits.1 as usize, digits.2 as usize);
+
+        self.v_registers[vx] ^= self.v_registers[vy];
+    }
+
+    fn op_8xy4(&mut self, digits: Digits) {
+        let (vx, vy) = (digits.1 as usize, digits.2 as usize);
+
+        let (new_vx, carry) = self.v_registers[vx].overflowing_add(self.v_registers[vy]);
+        let new_vf = if carry { 1 } else { 0 };
+
+        self.v_registers[vx] = new_vx;
+        self.v_registers[0xF] = new_vf;
+    }
+
+    fn op_8xy5(&mut self, digits: Digits) {
+        let (vx, vy) = (digits.1 as usize, digits.2 as usize);
+
+        let (new_vx, borrow) = self.v_registers[vx].overflowing_sub(self.v_registers[vy]);
+        let new_vf = if borrow { 0 } else { 1 };
+
+        self.v_registers[vx] = new_vx;
+        self.v_registers[0xF] = new_vf;
+    }
+
+    fn op_8xy6(&mut self, digits: Digits) {
         let (_, d2, _, _) = digits;
         let vx = d2 as usize;
+
         let lsb = self.v_registers[vx] & 1;
+
         self.v_registers[vx] >>= 1;
         self.v_registers[0xF] = lsb;
+    }
+
+    fn op_8xy7(&mut self, digits: Digits) {
+        let (_, d2, d3, _) = digits;
+        let (vx, vy) = (d2 as usize, d3 as usize);
+
+        let (new_vx, borrow) = self.v_registers[vy].overflowing_sub(self.v_registers[vx]);
+        let new_vf = if borrow { 0 } else { 1 };
+
+        self.v_registers[vx] = new_vx;
+        self.v_registers[0xF] = new_vf;
+    }
+
+    fn op_8xye(&mut self, digits: Digits) {
+        let (_, d2, _, _) = digits;
+        let vx = d2 as usize;
+        let msb = (self.v_registers[vx] >> 7) & 1;
+
+        self.v_registers[vx] <<= 1;
+        self.v_registers[0xF] = msb;
+    }
+
+    fn op_9xy0(&mut self, digits: Digits) {
+        let (_, d2, d3, _) = digits;
+        let (vx, vy) = (d2 as usize, d3 as usize);
+
+        if self.v_registers[vx] != self.v_registers[vy] {
+            self.program_counter += 2;
+        }
+    }
+
+    fn op_annn(&mut self, op: u16) {
+        self.i_register = op & 0xFFF;
+    }
+
+    fn op_bnnn(&mut self, op: u16) {
+        self.program_counter = (self.v_registers[0] as u16) + op & 0xFFF;
+    }
+
+    fn op_cxnn(&mut self, op: u16, digits: Digits) {
+        let vx = digits.1 as usize;
+        let rng: u8 = rand::random();
+        self.v_registers[vx] = rng & (op & 0xFF) as u8;
+    }
+
+    fn op_dxyn(&mut self, digits: Digits) {
+        let (_, d2, d3, d4) = digits;
+        let (vx, vy, num_rows) = (d2 as usize, d3 as usize, d4);
+
+        let x_coord = self.v_registers[vx] as u16;
+        let y_coord = self.v_registers[vy] as u16;
+
+        let mut flipped = false;
+
+        for y_line in 0..num_rows {
+            let address = self.i_register + y_line as u16;
+            let pixels = self.ram[address as usize];
+
+            for x_line in 0..8 {
+                if (pixels & (0b1000_0000 >> x_line)) != 0 {
+                    let x = (x_coord + x_line) as usize % SCREEN_WIDTH;
+
+                    let y = (y_coord + y_line) as usize % SCREEN_HEIGHT;
+
+                    let idx = x + SCREEN_WIDTH * y;
+
+                    flipped |= self.screen[idx];
+                    self.screen[idx] ^= true;
+                }
+            }
+        }
+
+        if flipped {
+            self.v_registers[0xF] = 1;
+        } else {
+            self.v_registers[0xF] = 0;
+        }
+    }
+
+    fn op_ex9e(&mut self, digits: Digits) {
+        let x = digits.1 as usize;
+        let vx = self.v_registers[x];
+        let key = self.keys[vx as usize];
+
+        if key {
+            self.program_counter += 2;
+        }
+    }
+
+    fn op_exa1(&mut self, digits: Digits) {
+        let x = digits.1 as usize;
+        let vx = self.v_registers[x];
+        let key = self.keys[vx as usize];
+
+        if !key {
+            self.program_counter += 2;
+        }
+    }
+
+    fn op_fx07(&mut self, digits: Digits) {
+        let x = digits.1 as usize;
+        self.v_registers[x] = self.delay_timer;
+    }
+
+    fn op_fx0a(&mut self, digits: Digits) {
+        let x = digits.1 as usize;
+        let mut pressed = false;
+
+        for i in 0..self.keys.len() {
+            if self.keys[i] {
+                self.v_registers[x] = i as u8;
+                pressed = true;
+                break;
+            }
+        }
+
+        if !pressed {
+            self.program_counter -= 2;
+        }
+    }
+
+    fn op_fx15(&mut self, digits: Digits) {
+        let x = digits.1 as usize;
+        self.delay_timer = self.v_registers[x];
+    }
+
+    fn op_fx18(&mut self, digits: Digits) {
+        let x = digits.1 as usize;
+        self.sound_timer = self.v_registers[x];
+    }
+
+    fn op_fx1e(&mut self, digits: Digits) {
+        let x = digits.1 as usize;
+        let vx = self.v_registers[x] as u16;
+        self.i_register = self.i_register.wrapping_add(vx);
+    }
+
+    fn op_fx29(&mut self, digits: Digits) {
+        let x = digits.1 as usize;
+        let c = self.v_registers[x] as u16;
+        self.i_register = c * 5;
+    }
+
+    fn op_fx33(&mut self, digits: Digits) {
+        let x = digits.1 as usize;
+        let vx = self.v_registers[x] as f32;
+
+        let hundreds = (vx / 100.0).floor() as u8;
+        let tens = ((vx / 10.0) % 10.0).floor() as u8;
+        let ones = (vx % 10.0) as u8;
+
+        self.ram[self.i_register as usize] = hundreds;
+        self.ram[(self.i_register + 1) as usize] = tens;
+        self.ram[(self.i_register + 2) as usize] = ones;
+    }
+
+    fn op_fx55(&mut self, digits: Digits) {
+        let x = digits.1 as usize;
+        let i = self.i_register as usize;
+
+        for idx in 0..=x {
+            self.ram[i + idx] = self.v_registers[idx];
+        }
+    }
+
+    fn op_fx65(&mut self, digits: Digits) {
+        let x = digits.1 as usize;
+        let i = self.i_register as usize;
+
+        for idx in 0..=x {
+            self.v_registers[idx] = self.ram[i + idx];
+        }
     }
 
     pub fn reset(&mut self) {
